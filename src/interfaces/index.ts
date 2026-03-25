5f7f8fd495e32450821cd114f62c09b5f1727f6d
insanetim <47120724+insanetim@users.noreply.github.com>
2026-03-25 16:52:28 +0200

    add get current weather endpoint

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -6,3 +6,55 @@ export interface GetCoordinatesResponse {
   country: string
   state: string
 }
+
+export interface GetCurrentWeatherResponse {
+  coord: {
+    lon: number
+    lat: number
+  }
+  weather: {
+    id: number
+    main: string
+    description: string
+    icon: string
+  }
+  base: string
+  main: {
+    temp: number
+    feels_like: number
+    pressure: number
+    humidity: number
+    temp_min: number
+    temp_max: number
+    sea_level: number
+    grnd_level: number
+  }
+  visibility: number
+  wind: {
+    speed: number
+    deg: number
+    gust: number
+  }
+  clouds: {
+    all: number
+  }
+  rain: {
+    "1h": number
+  }
+  snow: {
+    "1h": number
+  }
+  dt: number
+  sys: {
+    type: number
+    id: number
+    message: string
+    country: string
+    sunrise: number
+    sunset: number
+  }
+  timezone: number
+  id: number
+  name: string
+  cod: number
+}
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from "axios"
-import type { GetCoordinatesResponse } from "../interfaces"
+import type {
+  GetCoordinatesResponse,
+  GetCurrentWeatherResponse,
+} from "../interfaces"
 
 const BASE_URL = "http://api.openweathermap.org/"
 
@@ -27,6 +30,19 @@ client.interceptors.request.use(config => {
 export const getCoordinatesByLocationName = async (query: string) => {
   const response = await client.get<GetCoordinatesResponse[]>(
     `geo/1.0/direct?q=${query}&limit=5`
+  )
+  return response.data
+}
+
+export const getCurrentWeather = async ({
+  lat,
+  lon,
+}: {
+  lat: number
+  lon: number
+}) => {
+  const response = await client.get<GetCurrentWeatherResponse>(
+    `data/2.5/weather?lat=${lat}&lon=${lon}`
   )
   return response.data
 }
