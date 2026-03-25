aff5a1ffc401c00590d15b9d25711afc31c46adf
insanetim <47120724+insanetim@users.noreply.github.com>
2026-03-25 12:28:00 +0200

    add api client

---
+++ src/services/api.ts
@@ -0,0 +1,32 @@
+import axios from "axios"
+import type { GetCoordinatesResponse } from "../interfaces"
+
+const BASE_URL = "http://api.openweathermap.org/"
+
+const client = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+
+// Add request interceptor to append API key to all requests
+client.interceptors.request.use(config => {
+  const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY
+  if (apiKey) {
+    // Add appid parameter to existing params or create new params object
+    config.params = {
+      ...config.params,
+      appid: apiKey,
+    }
+  }
+  return config
+})
+
+export const getCoordinatesByLocationName = async (query: string) => {
+  const response = await client.get<GetCoordinatesResponse[]>(
+    `geo/1.0/direct?q=${query}&limit=5`
+  )
+  return response.data
+}
