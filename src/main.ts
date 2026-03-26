a34ed298b71c70b4844b468dd7a211f948e2aad0
insanetim <47120724+insanetim@users.noreply.github.com>
2026-03-26 14:16:36 +0200

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import "flag-icons/css/flag-icons.min.css"
+import { createPinia } from "pinia"
 import { createApp } from "vue"
 import App from "./App.vue"
 import "./style.css"
 
-createApp(App).mount("#app")
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+app.mount("#app")
