c69fc41a219ac0488dfd070d6e2d1d732f70005e
insanetim <insanetim1986@gmail.com>
2026-06-10 18:20:23 +0300

    feat: focus directive

--- src/components/CitySelect.vue
+++ src/components/CitySelect.vue
@@ -29,15 +29,16 @@ function edit() {
 
 <template>
   <div class="city-select">
-    <div v-show="isEdited" class="city-input">
+    <div v-if="isEdited" class="city-input">
       <Input
         v-model="city"
+        v-focus
         placeholder="Введите город"
         @keyup.enter="select()"
       />
       <Button @click="select()">Сохранить</Button>
     </div>
-    <Button v-show="!isEdited" @click="edit()">
+    <Button v-else @click="edit()">
       <IconLocation />
       Изменить город
     </Button>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.directive("focus", {
+  mounted(el) {
+    el.focus();
+  },
+});
+app.mount("#app");
