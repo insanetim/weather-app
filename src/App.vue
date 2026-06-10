6e40389bccf2d6400aa40e07ebefb22241ffff2a
insanetim <insanetim1986@gmail.com>
2026-06-10 22:35:37 +0300

    refactor: pane right

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,10 @@
 <script setup>
-import { computed, ref } from "vue";
-import CitySelect from "./components/CitySelect.vue";
-import DayCard from "./components/DayCard.vue";
-import Error from "./components/Error.vue";
-import Stat from "./components/Stat.vue";
-
-const errorMap = new Map([[1006, "Указанный город не найден"]]);
+import { ref } from "vue";
+import PaneRight from "./components/PaneRight.vue";
 
 const data = ref();
 const error = ref();
 const activeIndex = ref(0);
-
-const dataModified = computed(() => {
-  return [
-    {
-      label: "Влажность",
-      stat: data.value.current.humidity + " %",
-    },
-    {
-      label: "Облачность",
-      stat: data.value.current.cloud + " %",
-    },
-    {
-      label: "Ветер",
-      stat: data.value.current.wind_kph + " км/ч",
-    },
-  ];
-});
-const errorDisplay = computed(() => {
-  return errorMap.get(error.value?.error?.code);
-});
 
 async function getCity(city) {
   const params = new URLSearchParams({
@@ -54,27 +29,13 @@ async function getCity(city) {
 <template>
   <main class="main">
     <div class="left"></div>
-    <div class="right">
-      <Error :error="errorDisplay" />
-      <div v-if="data" class="stat-data">
-        <div class="stat-list">
-          <Stat v-for="item in dataModified" :key="item.label" v-bind="item" />
-        </div>
-        <div class="day-card-list">
-          <DayCard
-            v-for="(item, index) in data.forecast.forecastday"
-            :key="item.date"
-            :condition-icon="item.day.condition.icon"
-            :condition-text="item.day.condition.text"
-            :temp="item.day.avgtemp_c"
-            :date="new Date(item.date)"
-            :is-active="activeIndex === index"
-            @click="activeIndex = index"
-          />
-        </div>
-      </div>
-      <CitySelect @select-city="getCity" />
-    </div>
+    <PaneRight
+      :data
+      :error
+      :active-index="activeIndex"
+      @select-index="(i) => (activeIndex = i)"
+      @select-city="(city) => getCity(city)"
+    />
   </main>
 </template>
 
@@ -91,25 +52,5 @@ async function getCity(city) {
   background-image: url("./assets/bg.png");
   background-repeat: no-repeat;
   background-size: cover;
-}
-.right {
-  background: var(--color-bg-main);
-  padding: 60px 50px;
-  border-radius: 0 25px 25px 0;
-}
-.stat-data {
-  display: flex;
-  flex-direction: column;
-  gap: 80px;
-  margin-bottom: 70px;
-}
-.stat-list {
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-}
-.day-card-list {
-  display: flex;
-  gap: 1px;
 }
 </style>
---
+++ src/components/PaneRight.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { computed } from "vue";
+import CitySelect from "./CitySelect.vue";
+import DayCard from "./DayCard.vue";
+import Error from "./Error.vue";
+import Stat from "./Stat.vue";
+
+const { error, data, activeIndex } = defineProps({
+  error: Object,
+  data: Object,
+  activeIndex: Number,
+});
+
+const emit = defineEmits(["selectIndex", "selectCity"]);
+
+const errorMap = new Map([[1006, "Указанный город не найден"]]);
+
+const errorDisplay = computed(() => {
+  if (!error) return null;
+
+  return errorMap.get(error.error?.code) || error.error?.message;
+});
+
+const statData = computed(() => {
+  if (!data) return [];
+
+  return [
+    {
+      label: "Влажность",
+      stat: data.current.humidity + " %",
+    },
+    {
+      label: "Облачность",
+      stat: data.current.cloud + " %",
+    },
+    {
+      label: "Ветер",
+      stat: data.current.wind_kph + " км/ч",
+    },
+  ];
+});
+</script>
+
+<template>
+  <div class="right">
+    <Error :error="errorDisplay" />
+    <div v-if="data" class="stat-data">
+      <div class="stat-list">
+        <Stat v-for="item in statData" :key="item.label" v-bind="item" />
+      </div>
+      <div class="day-card-list">
+        <DayCard
+          v-for="(item, index) in data.forecast.forecastday"
+          :key="item.date"
+          :condition-icon="item.day.condition.icon"
+          :condition-text="item.day.condition.text"
+          :temp="item.day.avgtemp_c"
+          :date="new Date(item.date)"
+          :is-active="activeIndex === index"
+          @click="emit('selectIndex', index)"
+        />
+      </div>
+    </div>
+    <CitySelect @select-city="(city) => emit('selectCity', city)" />
+  </div>
+</template>
+
+<style scoped>
+.right {
+  background: var(--color-bg-main);
+  padding: 60px 50px;
+  border-radius: 0 25px 25px 0;
+}
+.stat-data {
+  display: flex;
+  flex-direction: column;
+  gap: 80px;
+  margin-bottom: 70px;
+}
+.stat-list {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+.day-card-list {
+  display: flex;
+  gap: 1px;
+}
+</style>
